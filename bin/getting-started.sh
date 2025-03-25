6d32a514118c2525276345327afcf8c6b3899f69
Sean Herbert <65455864+seanherbert105@users.noreply.github.com>
2025-03-25 12:28:14 -0400

    Update getting-started.sh

--- bin/getting-started.sh
+++ bin/getting-started.sh
@@ -2,8 +2,12 @@
 
 set -e
 
+# Build Jenkins container image with Docker
+echo "Building Jenkins with Docker..."
+docker buildx build -t jenkins/docker:latest -f Dockerfile.jenkins .
+
 # Define container names and images
-JENKINS_IMAGE="jenkins/jenkins"
+JENKINS_IMAGE="jenkins/docker:latest"
 TRIVY_IMAGE="aquasec/trivy"
 
 # Pull Jenkins image
@@ -13,6 +17,7 @@ docker pull $JENKINS_IMAGE
 # Run Jenkins container
 echo "Starting Jenkins container..."
 docker run -d --name jenkins -p 8080:8080 -p 50000:50000 \
+    -v /var/run/docker.sock:/var/run/docker.sock \
     -v jenkins_home:/var/jenkins_home \
     --restart unless-stopped $JENKINS_IMAGE
 
