769a1dac4feca4d60a5e8503da1da7bba8680457
Sean Herbert <65455864+seanherbert105@users.noreply.github.com>
2025-03-25 09:43:09 -0400

    Delete bin/install-kubectl.sh

--- bin/install-kubectl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# For AMD64 / x86_64
-[ $(uname -m) = x86_64 ] && curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-# For ARM64
-[ $(uname -m) = aarch64 ] && curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
-
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
