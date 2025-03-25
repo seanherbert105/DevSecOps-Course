bf39ee4b609f1284649e3a565b38eaa84d2a5cca
Sean Herbert <65455864+seanherbert105@users.noreply.github.com>
2025-03-25 13:31:15 -0400

    Update getting-started.sh

--- bin/getting-started.sh
+++ bin/getting-started.sh
@@ -13,6 +13,7 @@ TRIVY_IMAGE="aquasec/trivy"
 # Run Jenkins container
 echo "Starting Jenkins container..."
 docker run -d --name jenkins -p 8080:8080 -p 50000:50000 \
+    -v /usr/bin/docker:/usr/bin/docker \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v jenkins_home:/var/jenkins_home \
     --restart unless-stopped $JENKINS_IMAGE
