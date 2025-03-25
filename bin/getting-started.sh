a174dbd575a3dd415e0fca1454bce3968baadee4
Sean Herbert <65455864+seanherbert105@users.noreply.github.com>
2025-03-25 13:36:12 -0400

    Update getting-started.sh

--- bin/getting-started.sh
+++ bin/getting-started.sh
@@ -2,6 +2,24 @@
 
 set -e
 
+# Create Jenkins Docker network
+docker network create jenkins
+
+# Download and run Docker in Docker image 
+docker run \
+  --name jenkins-docker \
+  --rm \
+  --detach \
+  --privileged \
+  --network jenkins \
+  --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --publish 2376:2376 \
+  docker:dind \
+  --storage-driver overlay2
+
 # Build Jenkins container image with Docker
 echo "Building Jenkins with Docker..."
 docker buildx build -t jenkins/docker -f Dockerfile.jenkins .
@@ -12,11 +30,19 @@ TRIVY_IMAGE="aquasec/trivy"
 
 # Run Jenkins container
 echo "Starting Jenkins container..."
-docker run -d --name jenkins -p 8080:8080 -p 50000:50000 \
-    -v /usr/bin/docker:/usr/bin/docker \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v jenkins_home:/var/jenkins_home \
-    --restart unless-stopped $JENKINS_IMAGE
+docker run \
+  --name jenkins-blueocean \
+  --restart=on-failure \
+  --detach \
+  --network jenkins \
+  --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client \
+  --env DOCKER_TLS_VERIFY=1 \
+  --publish 8080:8080 \
+  --publish 50000:50000 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  jenkins/docker
 
 # Pull Trivy image
 echo "Pulling Trivy image..."
