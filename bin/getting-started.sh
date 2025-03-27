f4b00c4efd50e8ac3c339acb634503a182b9c580
Sean Herbert <65455864+seanherbert105@users.noreply.github.com>
2025-03-27 10:30:31 -0400

    Update getting-started.sh

--- bin/getting-started.sh
+++ bin/getting-started.sh
@@ -5,21 +5,6 @@ set -e
 # Create Jenkins Docker network
 docker network create jenkins
 
-# Download and run Docker in Docker image 
-docker run \
-  --name jenkins-docker \
-  --rm \
-  --detach \
-  --privileged \
-  --network jenkins \
-  --network-alias docker \
-  --env DOCKER_TLS_CERTDIR=/certs \
-  --volume jenkins-docker-certs:/certs/client \
-  --volume jenkins-data:/var/jenkins_home \
-  --publish 2376:2376 \
-  docker:dind \
-  --storage-driver overlay2
-
 # Build Jenkins container image with Docker
 echo "Building Jenkins with Docker..."
 docker buildx build -t jenkins/docker -f Dockerfile .
