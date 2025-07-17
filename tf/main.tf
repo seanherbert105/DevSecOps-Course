85ff07ac1072e7cce36421c4dc28357d3d680d57
root <root@DESKTOP-23MEHD7.>
2025-07-17 07:07:33 -0400

    fixing configmap reference.

--- tf/main.tf
+++ tf/main.tf
@@ -1,14 +1,31 @@
 terraform {
   required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.36.0"
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
     }
   }
 }
 
-provider "kubernetes" {
-  # This should be the default location where Docker Desktop stores your Kubernetes configuration.
-  # Please change to fit your exisiting Kubernetes configuration.
-  config_path = "~/.kube/config"
+provider "local" {}
+
+resource "local_file" "medieval_congrats" {
+  filename = "${path.module}/success.txt"
+
+  content  = <<EOT
+Hear ye, hear ye!
+
+Thou hast summoned the ancient and mystical powers of Terraform and forged thy very first deployment into the realm of DevOps.
+
+🎉 Huzzah! 🎉
+
+Let it be known across the kingdom, from the farthest node to the deepest pod, that henceforth thou art a DevOps sorcerer of the highest order!
+
+"And the Lord spake, saying, 'First shalt thou take out the Holy Pin, then shalt thou count to three, no more, no less...'" — *Monty Python and the Holy Grail*
+
+Go forth, noble engineer, and let your infrastructure be ever declarative.
+
+Signed,
+Sir Git of the Branch
+EOT
 }
